9cd5e1bd919aaafef9033dff7e1f19421f205d51
kerrechydy9841-glitch <k.errechydy9841@uca.ac.ma>
2025-11-26 20:07:44 +0100

    Update app.py
    Les modifications à app.py  /users permettent désormais de lister tous les utilisateurs (GET) et de créer un utilisateur en validant les données reçues (POST), avec un traitement approprié des erreurs et un retour JSON propre.

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def create_app(test_config=None):
     # Import models here so SQLAlchemy is aware of them before migrations
     # or ``create_all`` run. Students will flesh these out in ``models.py``.
     import models  # noqa: F401
+    from models import User, Post
 
     @app.route("/")
     def index():
@@ -39,6 +40,19 @@ def create_app(test_config=None):
 
     @app.route("/users", methods=["GET", "POST"])
     def users():
+        if request.method == "GET":
+            users = User.query.all()
+            return jsonify([u.to_dict() for u in users]), 200
+
+        if request.method == "POST":
+            data = request.get_json()
+            if not data or "username" not in data or "email" not in data:
+                return jsonify({"error": "Missing username or email"}), 400
+
+            new_user = User(username=data["username"], email=data["email"])
+            db.session.add(new_user)
+            db.session.commit()
+
         """List or create users.
 
         TODO: Students should query ``User`` objects, serialize them to JSON,
@@ -46,12 +60,34 @@ def create_app(test_config=None):
         """
 
         return (
-            jsonify({"message": "TODO: implement user listing/creation"}),
+            jsonify(new_user.to_dict()),
             501,
         )
 
     @app.route("/posts", methods=["GET", "POST"])
     def posts():
+       if request.method == "GET":
+            posts = Post.query.all()
+            return jsonify([p.to_dict() for p in posts]), 200
+
+        if request.method == "POST":
+            data = request.get_json()
+            if not data or "title" not in data or "content" not in data or "user_id" not in data:
+                return jsonify({"error": "Missing title, content or user_id"}), 400
+
+            # Vérifier si user existe
+            user = User.query.get(data["user_id"])
+            if not user:
+                return jsonify({"error": "User not found"}), 404
+
+            new_post = Post(
+                title=data["title"],
+                content=data["content"],
+                user_id=data["user_id"]
+            )
+            db.session.add(new_post)
+            db.session.commit()
+
         """List or create posts.
 
         TODO: Students should query ``Post`` objects, include user data, and
@@ -59,7 +95,7 @@ def create_app(test_config=None):
         """
 
         return (
-            jsonify({"message": "TODO: implement post listing/creation"}),
+            jsonify({new_post.to_dict()),
             501,
         )
 
